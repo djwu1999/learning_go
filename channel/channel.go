95be72b694a1a5b0f53781f74f4d2c378b6b2a21
djwu <790794992@qq.com>
2021-10-28 01:24:59 +0800

    update

--- channel/channel.go
+++ channel/channel.go
@@ -1,48 +1,81 @@
 package main
 
 import (
+	"container/heap"
 	"fmt"
-	"time"
+	rand2 "math/rand"
 )
 
-func createWorker (id int) chan<- int{
-	c := make(chan int)
-	go func() {
-		for {
-			fmt.Printf("Worker %d received %c\n",
-				id, <-c)
+type Phone struct {
+	number string
+	id int
+}
+
+func (phone *Phone) call(number string) {
+	fmt.Printf("I'm %s, please call the number %s, %d", phone.number, number, phone.id)
+}
+
+func findTargetSumWays(nums []int, target int) (count int) {
+	var helper func(index int, sum int)
+	helper = func(index int, sum int) {
+		if index == len(nums) {
+			if sum == target {
+				count++
+			}
+			return
 		}
-	}()
-	return c
+		helper(index + 1, sum + nums[index])
+		helper(index + 1, sum - nums[index])
+	}
+	helper(0, 0)
+	return
 }
 
+//func helper(nums []int, target int, index int) {
+//	if index == len(nums) {
+//		if sum == target {
+//			res++
+//		}
+//		return
+//	}
+//	sum += nums[index]
+//	helper(nums, target, index + 1)
+//	sum -= nums[index]
+//	sum -= nums[index]
+//	helper(nums, target, index + 1)
+//	sum += nums[index]
+//}
 
-func chanDemo() {
-	var channels [10]chan<- int
-	for i := 0; i < 10; i++ {
-		channels[i] = createWorker(i)
-	}
+type IntHeap []int
 
-	for i := 0; i < 10; i++ {
-		channels[i] <- 'a' + i
-	}
+func (h IntHeap) Len() int           { return len(h) }
+func (h IntHeap) Less(i, j int) bool { return h[i] < h[j] }
+func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
 
-	for i := 0; i < 10; i++ {
-		channels[i] <- 'A' + i
-	}
-
-	for i := 0; i < 10; i++ {
-		channels[i] <- 'a' + i
-	}
-	time.Sleep(time.Millisecond)
+func (h *IntHeap) Push(x interface{}) {
+	// Push and Pop use pointer receivers because they modify the slice's length,
+	// not just its contents.
+	*h = append(*h, x.(int))
 }
 
-func bufferedChannel() {
-	c := make(chan int)
-
-	c <- 1
+func (h *IntHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
 }
 
 func main() {
-
+	//heap := new(heap.Interface)=
+	h := &IntHeap{12, 123, 312}
+	for i := 0; i < 12; i++ {
+		num := rand2.Intn(1000)
+		heap.Push(h, num)
+	}
+	for h.Len() > 0{
+		num := heap.Pop(h)
+		fmt.Println(num)
+	}
+	fmt.Println(h.Len())
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module learning_go
 
 go 1.16
+
+require github.com/gin-gonic/gin v1.7.4 // indirect
